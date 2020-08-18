269b23750204bf2777124a865859f53f180ccc0a
Cleilson <marcialwushu@gmail.com>
2020-08-18 19:21:46 -0300

    metodo de registro na ApplicationUserController adicionado

--- WebAPI/WebLoginApplication/Controllers/ApplicationUserController.cs
+++ WebAPI/WebLoginApplication/Controllers/ApplicationUserController.cs
@@ -1,11 +1,49 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebLoginApplication.Models;
 
 namespace WebLoginApplication.Controllers
 {
-    public class ApplicationUserController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
     {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _singInManager;
+        private readonly ApplicationSettings _appSettings;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> singInManager, ApplicationSettings appSettings)
+        {
+            _userManager = userManager;
+            _singInManager = singInManager;
+            _appSettings = appSettings;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        //POST : /api/Application/Register
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
