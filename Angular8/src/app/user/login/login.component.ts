079cced08ec5c2cbb2438a91814f46fde1d99ca0
Cleilson <marcialwushu@gmail.com>
2020-08-19 16:29:10 -0300

    Metodo de submit e tratamento de erro no Login com NgForm em LoginComponent

--- Angular8/src/app/user/login/login.component.ts
+++ Angular8/src/app/user/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../../shared/user.service';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +13,35 @@ export class LoginComponent implements OnInit {
 
   imagePath = '../../../assets/img/user-logo.png';
 
-  constructor(private router: Router) { }
+  formModel = {
+    UserName: '',
+    Password: ''
+  };
+
+  constructor(private router: Router, private service: UserService, private toastr: ToastrService) { }
 
   ngOnInit() {
+    if (localStorage.getItem('token') != null) {
+      this.router.navigateByUrl('/home');
+    }
   }
 
-  onSubmit() {
-    this.router.navigateByUrl('/home');
+  onSubmit(form: NgForm) {
+    this.service.login(form.value).subscribe(
+      (res: any) => {
+        localStorage.setItem('token', res.token);
+        this.router.navigateByUrl('/home');
+      },
+      err => {
+        if (err.status === 400) {
+          this.toastr.error('Username ou Password incorretos', 'Authentication failed');
+        } else {
+          console.log(err);
+        }
+      }
+    );
+
+
   }
 
 }
