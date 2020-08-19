6b373659c37c8a79a5fc18f8d7a03c1ecdbf3271
Cleilson <marcialwushu@gmail.com>
2020-08-18 23:46:49 -0300

    Adicionado componentes User | Login | Registration
    configurado Rotas para componente Login por default

--- Angular8/src/app/app.module.ts
+++ Angular8/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegistrationComponent } from './user/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
